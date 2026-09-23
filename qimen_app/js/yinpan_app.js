fbe88b72c402a0d046bc5eb1e5276a799968e04f
wrz <stripsss@live.com>
2026-09-24 03:54:49 +0800

    chore: 版本号同步到 v1.4.3

--- qimen_app/js/yinpan_app.js
+++ qimen_app/js/yinpan_app.js
@@ -16156,7 +16156,7 @@ function _renderBottomBar() {
 }
 
 // === 关于弹窗 ===
-const APP_VERSION = '1.4.2';
+const APP_VERSION = '1.4.3';
 const APP_AUTHOR = '地天泰';
 const APP_REPO = 'github.com/wrz1911/daojiayinpan';
 function showAbout() {
